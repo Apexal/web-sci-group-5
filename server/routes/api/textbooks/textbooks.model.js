609ebd9027419b8ea4c6028a2bd271e0ba497b2b
KevTLW <kevinmcruzc@gmail.com>
2020-03-01 23:36:11 -0500

    Update Textbook model to better match results from the bookstore API

--- server/routes/api/textbooks/textbooks.model.js
+++ server/routes/api/textbooks/textbooks.model.js
@@ -4,11 +4,12 @@ const Schema = mongoose.Schema;
 // https://github.com/Apexal/web-sci-group-5/wiki/Textbooks
 const schema = new Schema(
     {
+        image: { type: String },
         title: { type: String, required: true, minlength: 3, maxlength: 200 },
-        isbn: { type: String, required: true, minlength: 7, maxlength: 10 },
+        isbn: { type: String, required: true, minlength: 10, maxlength: 17 },
         authors: { type: String, required: true, minlength: 1, maxlength: 100 },
         publisher: { type: String, required: true, minlength: 1, maxlength: 100 },
-        edition: { type: Number, required: true, default: 1 },
+        edition: { type: String, required: true, default: "1st" },
         retailPrice: { type: Number }
     },
     {
