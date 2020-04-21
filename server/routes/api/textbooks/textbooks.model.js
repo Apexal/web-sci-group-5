0047e30d4fd81b488cd2d240b7d6e18dbfd70c04
Frank Matranga <thefrankmatranga@gmail.com>
2020-04-21 13:19:35 -0400

    Added props to Textbook model

--- server/routes/api/textbooks/textbooks.model.js
+++ server/routes/api/textbooks/textbooks.model.js
@@ -6,8 +6,12 @@ const schema = new Schema(
     {
         image: { type: String },
         title: { type: String, required: true, minlength: 3, maxlength: 200 },
+        subtitle: { type: String, maxlength: 500 },
+        description: { type: String, maxlength: 2000 },
+        categories: [{ type: String }],
         isbn: { type: String, required: true, minlength: 10, maxlength: 17 },
         authors: { type: String, required: true, minlength: 1, maxlength: 100 },
+        pageCount: { type: Number, minlength: 1 },
         publisher: { type: String, required: true, minlength: 1, maxlength: 100 },
         edition: { type: String, required: true, default: "1st" },
         retailPrice: { type: Number }
