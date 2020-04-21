f006ed1232e2983eb305a908dc24ece69fa9ecce
KevTLW <kevinmcruzc@gmail.com>
2020-04-21 17:13:06 -0400

    Finished main page (Excluding link to actual listings

--- client/app/pages/main/main.js
+++ client/app/pages/main/main.js
@@ -4,11 +4,27 @@ angular
   .module("BookIt.main", ["ngRoute"])
   .config([
     "$routeProvider",
-    function($routeProvider) {
+    function ($routeProvider) {
       $routeProvider.when("/main", {
         templateUrl: "pages/main/main.html",
         controller: "MainCtrl"
       });
     }
   ])
-  .controller("MainCtrl", [function() {}]);
+  .controller("MainCtrl", ["$scope", "TextbookListingsService", function ($scope, textbookListingsService) {
+    $scope.getDate = function (createdAt) {
+      const now = new Date();
+      const creation = new Date(createdAt);
+
+      const diff = now.getTime() - creation.getTime();
+
+      return Math.floor(diff / (1000 * 3600 * 24));
+    }
+
+    $scope.textbookListings = [];
+    textbookListingsService.fetchTextbookListings()
+      .then(listings => {
+        $scope.textbookListings = listings;
+        $scope.$apply();
+      });
+  }]);
