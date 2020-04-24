0691a1c181ac0b85cc3b9927824310de68a84eb9
Frank Matranga <thefrankmatranga@gmail.com>
2020-04-24 10:50:05 -0400

    Cards

--- client/app/services/stripe.js
+++ client/app/services/stripe.js
@@ -5,25 +5,22 @@ angular.module("Stripe")
   ["$rootScope", "AuthService", function ($rootScope, AuthService) {
     const service = {};
     let stripe = null;
-    let elements = null;
 
     const initalizeWithUser = function () {
       if (AuthService.isAuthenticated()) {
         stripe = Stripe('pk_test_2AWcc2JXkSdRmMfyuOaIOiT300aORiftsc', /*/{
           stripeAccount: AuthService.getUser().stripeAccountID
         }*/);
-        elements = stripe.elements();
         console.log("Initialized Stripe and elements");
       } else {
         stripe = null;
-        elements = null;
       }
     }
 
     $rootScope.$on("user-changed", initalizeWithUser);
 
     service.stripe = () => stripe;
-    service.elements = () => elements;
+    service.elements = () => stripe.elements();
 
     return service;
   }]);
